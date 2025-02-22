8074fa7947e0050cc2c06b2f6182874fd2849dfd
unknown <novot129@fjfi.cvut.cz>
2025-02-22 17:54:00 +0100

    navbar thingy and footer thingy

---
+++ js/footer-elem.js
@@ -0,0 +1,68 @@
+﻿const footerElemTemplate = document.createElement('template');
+
+footerElemTemplate.innerHTML = `
+<div class="footer navtext">
+	<div class="quoteDisplayBox">Loading...</div>
+</div>
+`;
+
+
+class FooterElem extends HTMLElement {
+
+	constructor() {
+		super();
+
+		const shadow = this.attachShadow({mode: 'open'});
+		shadow.appendChild(footerElemTemplate.content.cloneNode(true));
+
+		const styleMainLink = document.createElement('link');
+		styleMainLink.rel = 'stylesheet';
+		styleMainLink.href = 'css/styles.css';
+		shadow.appendChild(styleMainLink)
+
+		const styleOtherLink = document.createElement('link');
+		styleOtherLink.rel = 'stylesheet';
+		styleOtherLink.href = 'css/ostatni.css';
+		shadow.appendChild(styleOtherLink);
+	}
+
+	quoteOnChange() {
+
+		const quotes = [
+			"„Jenom život, který žijeme pro ostatní, stojí za to.“ — Albert Einstein",
+			"„Všichni víme, že světlo putuje rychleji než zvuk. Proto někteří lidé vypadají chytře, dokud je neslyšíte mluvit.“ — Albert Einstein",
+			"„Svět je nebezpečné místo k životu, ne kvůli lidem, kteří jsou zlí, ale kvůli lidem, kteří s tím nic neudělají.“ — Albert Einstein",
+			"„První láska je jako los, který obvykle nevyhrává, ale jeho číslo si pamatujeme celý život.“ — Émile Zola",
+			"„Pokud se lidé dokážou mít alespoň trochu rádi, mohou být šťastní.“ — Émile Zola",
+			"„Sex bez lásky je prázdný zážitek. Ale ze všech prázdných zážitků je to jeden z nejlepších.“ — Woody Allen",
+			"„Sex uvolňuje napětí. Láska ho způsobuje.“ — Woody Allen",
+			"„Přemýšlíme příliš a cítíme málo.“ — Charlie Chaplin",
+			"„Tvé nahé tělo by mělo patřit jedině těm, kteří se zamilují do tvé nahé duše.“ — Charlie Chaplin",
+			"„Člověk raději poznává vesmír než sebe sama.“ — Ernest Hemingway",
+			"„Jsem si jistý, že vesmír je plný inteligentního života. Je však příliš inteligentní na to, aby sem přišel.“ — Arthur C. Clarke",
+			"„Když se člověk ptá: „Miluji vůbec?“, odpověď je vždy ne.“ — Arthur C. Clarke",
+			"„Život nás požírá, my život utrácíme - kdo z nás bude s tím druhým dříve hotov?“ — Josef Čapek",
+			"„Musíte v něco věřit: instinkt, osud, život, karmu, cokoliv.“ — Steve Jobs",
+			"„Nemůžete se jednoduše zeptat zákazníků, co chtějí, a pak se to pokusit sestavit. V době, kdy to budete mít hotové, už budou chtít něco jiného.“ — Steve Jobs",
+			"„Nejhorší zbraní škodolibosti je posměch, nejvzácnějším darem lásky je porozumění.“ — Otto František Babler",
+			"„Chceme-li si splnit sen, musíme o něm snít.“ — Walt Disney",
+			"„Když o tom dokážeš snít, dokážeš to udělat.“ — Walt Disney",
+			"„Vždycky jsem hrdý na to, jak dokážu milovat svět - nenávidět ho je totiž mnohem snazší.“ — Jack Kerouac",
+			"„Život musí být bohatý a plný lásky - jinak není dobrý, není dobrý vůbec, pro nikoho.“ — Jack Kerouac",
+		];
+
+		const randomIndex = Math.floor(Math.random() * quotes.length);
+		this.shadowRoot.querySelector('.quoteDisplayBox').innerText = quotes[randomIndex];
+	}
+
+	connectedCallback() {
+
+		this.quoteOnChange();
+
+		// 120000 = 2 min
+		setInterval(this.quoteOnChange, 120000);
+	}
+
+}
+
+customElements.define('footer-elem', FooterElem);
---
+++ js/navbar.js
@@ -0,0 +1,108 @@
+﻿
+// Get full path of current script
+const actScriptPath = new URL(document.currentScript.src).pathname;
+// Get only directory path from full path of current script
+const basePath = actScriptPath.substring(0, actScriptPath.lastIndexOf('/'));
+
+const navbarTemplate = document.createElement('template');
+navbarTemplate.innerHTML = `
+
+ <div class="menu">
+    <a href="https://fjfi.cvut.cz/cz/" class="logo"></a>
+    <div class="navtext">Stránky Ing. P. Eichlera, PhD</div>
+    <input type="checkbox" id="menu-bar">
+    <label for="menu-bar">Menu</label>
+    <div class="navbar">
+        <ul>
+            <li><a href="index.html">Domů</a></li>
+            <li>
+                <a href="#">O mně</a>
+                <ul>
+                    <span class="small_separator"></span>
+                    <li><a href="medajlonka.html">Info</a></li>
+                    <span class="small_separator"></span>
+                    <li><a href="kratkeCV.html">CV</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Výuka</a>
+                <ul>
+                    <span class="small_separator"></span>
+                    <li><a href="predmety.html">Vyučované předměty</a></li>
+                    <span class="small_separator"></span>
+                    <li><a href="rozvrh.html">Rozvrh</a></li>
+                    <span class="small_separator"></span>
+                    <li><a href="temata.html">Témata prací</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Výzkum</a>
+                <ul>
+                    <span class="small_separator"></span>
+                    <li><a href="publikace.html">Publikace</a></li>
+                    <span class="small_separator"></span>
+                    <li><a href="konference.html">Konference</a></li>
+                    <span class="small_separator"></span>
+                    <li><a href="vyzkumneprojekty.html">Výzkumné projekty</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
+`;
+
+
+class NavigationBar extends HTMLElement {
+	constructor(){
+		super();
+	}
+
+	connectedCallback() {
+
+		const shadowRoot = this.attachShadow({mode: 'open'});
+		shadowRoot.appendChild(navbarTemplate.content.cloneNode(true));
+
+        const styleMainLink = document.createElement('link');
+		styleMainLink.rel = 'stylesheet';
+		styleMainLink.href = 'css/styles.css';
+		shadowRoot.appendChild(styleMainLink)
+
+        const styleOtherLink = document.createElement('link');
+		styleOtherLink.rel = 'stylesheet';
+		styleOtherLink.href = 'css/ostatni.css';
+		shadowRoot.appendChild(styleOtherLink)
+
+	}
+}
+
+customElements.define('navigation-bar', NavigationBar);
+
+// function adjustNavbar() {
+
+//  console.log("Neco")
+
+//  const hamburger = document.createElement("button");
+//  hamburger.classList.add("hamburger");
+//  hamburger.textContent = "☰";
+//  document.querySelector("header").prepend(hamburger);
+
+//  hamburger.addEventListener("click", function() {
+//    const menu = document.getElementById("menu");
+//    menu.classList.toggle("show");
+//  });
+// }
+
+// document.addEventListener("DOMContentLoaded", adjustNavbar);
+
+// function loadNavbar() {
+//
+//	const navbarData = 'navbar.html'
+//	const navbarPlaceholder = 'navbar-placeholder'
+
+//	fetch(navbarData)
+//	.then(response => response.text())
+//	.then(data => {
+//		document.getElementById(navbarPlaceholder).innerHTML = data;
+//	});
+// }
+// document.addEventListener('DOMContentLoaded', loadNavbar);
